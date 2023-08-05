ee95398008bd80ed84251f5418766ed08b4265a4
amir sediqi <amir.sediqi69@gmail.com>
2023-08-05 18:24:36 +0200

    clean up

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -6,27 +6,60 @@ using System.Web.Mvc;
 using Vidly.Models;
 namespace Vidly.Controllers
 {
+    //    public class CustomersController : Controller
+    //    {
+    //        // GET: /Customers/
+    //        public ActionResult Index()
+    //        {
+    //            // Create a list of customers
+    //            var customers = new List<Customer>
+    //        {
+    //            new Customer { Id = 1, Name = "Customer 1" },
+    //            new Customer { Id = 2, Name = "Customer 2" }
+    //        };
+
+    //            return View(customers);
+    //        }
+    //        // details action method
+    //        public ActionResult Details(int id)
+    //        {
+
+    //            var customer = new Customer { Id = id, Name = "Sample Customer" };
+
+    //            return View(customer);
+    //        }
+    //    }
+    //}
+
     public class CustomersController : Controller
     {
-        // GET: /Customers/
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            // Create a list of customers
-            var customers = new List<Customer>
-        {
-            new Customer { Id = 1, Name = "Customer 1" },
-            new Customer { Id = 2, Name = "Customer 2" }
-        };
+            var customers = GetCustomers();
 
             return View(customers);
         }
-        // details action method
+
         public ActionResult Details(int id)
         {
-            
-            var customer = new Customer { Id = id, Name = "Sample Customer" };
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
 
             return View(customer);
         }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
     }
 }
+
+//
+
